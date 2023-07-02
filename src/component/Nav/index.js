cd9014f8e35a2e3136e2bd44a3d779aaec871880
Vijay <vijayrocky648@gmail.com>
2023-07-02 19:22:02 +0530

    add 3d text in my portfolio

--- src/component/Nav/index.js
+++ src/component/Nav/index.js
@@ -1,7 +1,7 @@
 import './index.css'
 
 function NAVBAR() {
-    return ( <nav class="navbar navbar-expand-lg customsettingnav nav " >
+    return ( <nav class="navbar navbar-expand-lg customsettingnav nav" >
     <div class="container">  
      <a class="navbar-brand" href="#"></a>   
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
